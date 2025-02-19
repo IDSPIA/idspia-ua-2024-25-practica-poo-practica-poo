8a6dac7b710b0ad3fe248b5b083c830dcf5340af
shillianne <vshiwork@gmail.com>
2025-02-19 17:46:59 +0100

    Updated project code to pass all tests.

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,5 +1,6 @@
 from typing import List
 from .dataset import DataSet
+import logging
 from .util import Util
 import os
 
@@ -16,13 +17,18 @@ class DataLoader:
             if not os.path.isdir(os.path.join(folder_path, subfolder)):
                 continue
             files = os.listdir(os.path.join(folder_path, subfolder))
-            dataset = DataSet(subfolder)
-            for file in files:
-                with open(os.path.join(folder_path, subfolder, file), "r") as f:
-                    for line in f:
-                        row = Util.parse_line(line[:-1], self.__separator)
-                        if row is not None and len(row) == 5:
-                            dataset.add_entry(row)
-            datasets.append(dataset)
+            if files:
+                dataset = DataSet(subfolder)
+                for file in files:
+                    with open(os.path.join(folder_path, subfolder, file), "r") as f:
+                        for line in f:
+                            row = Util.parse_line(line[:-1], self.__separator)
+                            if row is not None:
+                                dataset.add_entry(row)
+                            else:
+                                logging.getLogger('data.dataloader').warning("Línea inválida")
+                datasets.append(dataset)
+            else:
+                logging.getLogger('data.dataloader').warning("No se encontraron datos válidos")
 
         return datasets
--- src/data/util.py
+++ src/data/util.py
@@ -1,11 +1,19 @@
+import logging
+
 class Util:
     @staticmethod
     def parse_line(line: str, separator: str = " "):
+        if line == "":
+            logging.getLogger("data.util").warning("Línea vacía encontrada. Será ignorada.")
+            return None
         try:
             data = [float(v) for v in line.split(separator)]
-        except ValueError as e:
-            print("Couldn't properly load the data")
-            # What are the conditions for failure (i.e just non numerical data or also too much or too little data
+            assert len(data) == 5
+        except ValueError:
+            logging.getLogger('data.util').warning("Línea con valores no numéricos")
+            return None
+        except AssertionError:
+            logging.getLogger('data.util').warning("Línea con número incorrecto de valores")
             return None
         else:
             return data
